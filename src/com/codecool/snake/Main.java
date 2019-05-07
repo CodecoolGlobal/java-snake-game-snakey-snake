9bef7cd1e9af5da187cebc7a111742f0d245f015
kincsojerne <44428001+kincsojerne@users.noreply.github.com>
2019-05-07 16:17:50 +0200

    Revert "Feature/restart"

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -15,7 +15,6 @@ public class Main extends Application {
         Game game = new Game();
         Scene mainScene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
 
-
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(mainScene);
         primaryStage.show();
@@ -28,5 +27,3 @@ public class Main extends Application {
         System.out.println("Exiting..");
     }
 }
-
-
